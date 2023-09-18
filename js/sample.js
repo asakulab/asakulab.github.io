7a812b744d3bb013e087d062e63f14e5357af04f
asakulab <678.asakura@gmail.com>
2023-09-18 13:32:13 +0900

    🖥

--- js/sample.js
+++ js/sample.js
@@ -23,3 +23,72 @@ resetButton.addEventListener("click", function () {
 function updateCounterDisplay() {
     counterDisplay.textContent = counterValue;
 }
+
+
+
+// ストップウォッチ
+        let stopwatchTimer;
+        let hoursValue = 0;
+        let minutesValue = 0;
+        let secondsValue = 0;
+
+        const updateStopwatch = () => {
+            document.getElementById('hoursDisplay').textContent = String(hoursValue).padStart(2, '0');
+            document.getElementById('minutesDisplay').textContent = String(minutesValue).padStart(2, '0');
+            document.getElementById('secondsDisplay').textContent = String(secondsValue).padStart(2, '0');
+        };
+
+        document.getElementById('startButton').addEventListener('click', () => {
+            stopwatchTimer = setInterval(() => {
+                secondsValue++;
+                if (secondsValue === 60) {
+                    secondsValue = 0;
+                    minutesValue++;
+                    if (minutesValue === 60) {
+                        minutesValue = 0;
+                        hoursValue++;
+                    }
+                }
+                updateStopwatch();
+            }, 1000);
+        });
+
+        document.getElementById('stopButton').addEventListener('click', () => {
+            clearInterval(stopwatchTimer);
+        });
+
+        document.getElementById('resetButton').addEventListener('click', () => {
+            clearInterval(stopwatchTimer);
+            hoursValue = 0;
+            minutesValue = 0;
+            secondsValue = 0;
+            updateStopwatch();
+        });
+        document.getElementById('resetButton2').addEventListener('click', () => {
+            clearInterval(stopwatchTimer);
+            hoursValue = 0;
+            minutesValue = 0;
+            secondsValue = 0;
+            updateStopwatch();
+        });
+        
+
+//時間表示
+function updateTime() {
+    const hoursElement = document.getElementById("hours");
+    const minutesElement = document.getElementById("minutes");
+    const secondsElement = document.getElementById("seconds");
+    const currentTime = new Date();
+    const hours = currentTime.getHours().toString().padStart(2, "0");
+    const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+    const seconds = currentTime.getSeconds().toString().padStart(2, "0");
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+}
+
+                // ページが読み込まれたら初期表示を行う
+                updateTime();
+
+                // 1秒ごとに時間を更新する
+                setInterval(updateTime, 1000);
