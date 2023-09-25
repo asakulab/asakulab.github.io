3669e0f7b644b5067e68cb86cd53464fa9e0b34a
asakulab <678.asakura@gmail.com>
2023-09-25 13:58:37 +0900

    🖌２

--- js/oekaki.js
+++ js/oekaki.js
@@ -1,54 +1,51 @@
-// script.js
-
-const canvas = document.getElementById('drawingCanvas');
-const ctx = canvas.getContext('2d');
-const colorPicker = document.getElementById('colorPicker');
-const brushSize = document.getElementById('brushSize');
-const clearButton = document.getElementById('clearButton');
-
+let canvas = document.getElementById("drawArea");
+let ctx = canvas.getContext("2d");
 let drawing = false;
 
-canvas.addEventListener('mousedown', () => {
+// 線の設定
+ctx.lineWidth = 5;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'black';
+
+// マウス操作のイベント
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mousemove", draw);
+
+// タッチ操作のイベント
+canvas.addEventListener("touchstart", startDrawing);
+canvas.addEventListener("touchend", stopDrawing);
+canvas.addEventListener("touchmove", function(e) {
+    let touch = e.touches[0];
+    draw({clientX: touch.clientX, clientY: touch.clientY});
+});
+
+function startDrawing() {
     drawing = true;
-    ctx.beginPath();
-});
-
-canvas.addEventListener('mouseup', () => {
-    drawing = false;
-    ctx.closePath();
-});
-
-canvas.addEventListener('mousemove', draw);
-
-function draw(e) {
-    if (!drawing) return;
-
-    ctx.lineWidth = brushSize.value;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = colorPicker.value;
-
-    ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-    ctx.stroke();
 }
 
-clearButton.addEventListener('click', () => {
+function stopDrawing() {
+    drawing = false;
+    ctx.beginPath();
+}
+
+function draw(event) {
+    if (!drawing) return;
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+document.getElementById('clearButton').addEventListener('click', function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-
-canvas.addEventListener('touchstart', () => {
-    drawing = true;
-    ctx.beginPath();
-});
-
-canvas.addEventListener('touchend', () => {
-    drawing = false;
-    ctx.closePath();
-});
-
-canvas.addEventListener('touchmove', draw);
-
-ctx.lineTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
