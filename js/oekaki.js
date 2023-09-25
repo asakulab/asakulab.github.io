3ddfb8082215f5641b5eb16f100541fb5106b15c
asakulab <678.asakura@gmail.com>
2023-09-25 13:37:54 +0900

    1.3.3

---
+++ js/oekaki.js
@@ -0,0 +1,36 @@
+// script.js
+
+const canvas = document.getElementById('drawingCanvas');
+const ctx = canvas.getContext('2d');
+const colorPicker = document.getElementById('colorPicker');
+const brushSize = document.getElementById('brushSize');
+const clearButton = document.getElementById('clearButton');
+
+let drawing = false;
+
+canvas.addEventListener('mousedown', () => {
+    drawing = true;
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+    ctx.closePath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
+function draw(e) {
+    if (!drawing) return;
+
+    ctx.lineWidth = brushSize.value;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = colorPicker.value;
+
+    ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+    ctx.stroke();
+}
+
+clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
