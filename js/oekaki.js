a6769137b8e50456ece00ed9b9ee2b69cff2bc59
asakulab <678.asakura@gmail.com>
2023-09-25 14:10:18 +0900

    🖌

--- js/oekaki.js
+++ js/oekaki.js
@@ -11,12 +11,20 @@ ctx.strokeStyle = 'black';
 // マウス操作のイベント
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseout", stopDrawing);  // 追加: キャンバス外に出たときのイベント
 canvas.addEventListener("mousemove", draw);
 
 // タッチ操作のイベント
-canvas.addEventListener("touchstart", startDrawing);
-canvas.addEventListener("touchend", stopDrawing);
+canvas.addEventListener("touchstart", (e) => { 
+    e.preventDefault();
+    startDrawing();
+});
+canvas.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    stopDrawing();
+});
 canvas.addEventListener("touchmove", function(e) {
+    e.preventDefault();  // 追加: タッチ操作のデフォルト動作を抑制
     let touch = e.touches[0];
     draw({clientX: touch.clientX, clientY: touch.clientY});
 });
@@ -43,24 +51,6 @@ function draw(event) {
     ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 }
 
-document.getElementById('clearButton').addEventListener('click', function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
-
-// タッチ操作のイベント
-canvas.addEventListener("touchstart", (e) => { 
-    e.preventDefault();  // これを追加
-    startDrawing();
-});
-canvas.addEventListener("touchend", (e) => {
-    e.preventDefault();
-    stopDrawing();
-});
-canvas.addEventListener("touchmove", function(e) {
-    e.preventDefault();  // これは既にあるはず
-    let touch = e.touches[0];
-    draw({clientX: touch.clientX, clientY: touch.clientY});
-});
