1b55696a5d00753b5585a4d7b1267198cdc2ea24
asakulab <678.asakura@gmail.com>
2024-10-28 05:54:12 +0900

    BottomNav微修正

--- src/scripts/components/settings/bottom.js
+++ src/scripts/components/settings/bottom.js
@@ -1,8 +1,8 @@
 export const bottomNavItems = [
-    { "href": "/index.html",                "icon": "bi-house-door"     },
-    { "href": "/1.introduction/index.html", "icon": "bi-1-square-fill"  },
-    { "href": "/2.topic1/index.html",       "icon": "bi-2-square-fill"  },
-    { "href": "/3.topic2/index.html",       "icon": "bi-3-square-fill"  },
-    { "href": "/4.discussion/index.html",   "icon": "bi-4-square-fill"  },
-    { "href": "/5.message/index.html",      "icon": "bi-envelope"       }
+    { "href": "/index.html",            "icon": "bi-house-door",    },
+    { "href": "/1.profile/index.html",  "icon": "bi-person-circle", },
+    { "href": "/2.news/index.html",     "icon": "bi-newspaper",     },
+    { "href": "/3.works/index.html",    "icon": "bi-stack-overflow",},
+    { "href": "/4.logs/index.html",     "icon": "bi-folder-fill",   },
+    { "href": "/5.message/index.html",      "icon": "bi-envelope"   }
 ];
